1513e6ba6b5d9fd14157588b13bd1e44fd6fed82
jorgecastro31 <jorgecastroinaudyest@gmail.com>
2021-10-11 19:19:04 -0500

    branch_test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: React.FC = () => {
       <div>
         <div className="mt-1 relative rounded-md">
           <input
-            type="text"
+            type="text2"
             name="price"
             id="price"
             className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
